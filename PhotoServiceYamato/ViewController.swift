1bded87c329920acd7df73ab4eb0a5adf07dd5fd
Saito <wow.yusuke.saito@gmail.com>
2017-10-20 14:50:20 +0900

    Initial Commit

---
+++ PhotoServiceYamato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoServiceYamato
+//
+//  Created by pc131101 on 2017/10/20.
+//  Copyright © 2017年 HIDEYOSHI YAMATO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
