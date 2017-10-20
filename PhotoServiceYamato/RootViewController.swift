06d8177b7cb4403bcb98e07e84bb4c049c880a77
Saito <wow.yusuke.saito@gmail.com>
2017-10-20 17:47:22 +0900

    StartViewController追加

---
+++ PhotoServiceYamato/RootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewController.swift
+//  PhotoServiceYamato
+//
+//  Created by pc131101 on 2017/10/20.
+//  Copyright © 2017年 HIDEYOSHI YAMATO. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.blue // 背景を赤に
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PhotoServiceYamato/StartViewController.swift
@@ -0,0 +1,34 @@
+//
+//  StartViewController.swift
+//  PhotoServiceYamato
+//
+//  Created by pc131101 on 2017/10/20.
+//  Copyright © 2017年 HIDEYOSHI YAMATO. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
